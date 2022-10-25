e3a30466d84cf10cf494828b1225f6c639fe71a7
symsym <symeon.soullard@gmail.com>
2022-10-25 18:38:33 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,82 @@
+//Discord
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const prefix = ";"
+const ytdl = require("ytdl-core");
+
+var embedt = new Discord.MessageEmbed()
+  .setColor("#00F5FF")
+  .setTitle("oui mon fuhrer ?")
+  .setAuthor("Linkbot")
+
+
+var embedh = new Discord.MessageEmbed()
+  .setColor("#00F5FF")
+  .setTitle("**Commandes du Linkbot**")
+  .setAuthor("Linkbot")
+  .setDescription("**- Le prefix du bot est ;** \n - ; test : le bot vous répond pour dire qu'il fonctionne \n - ; help : affiche toutes les commandes du bot \n - ; punchline : le bot vous renvoie une punchline gratuite pour le plaisir. Il sera possible de cibler un utilisateur dans le futur (`une punchline disponible pour le moment`) \n - ;clear [nombre] : permet de supprimer autant de messages que le nombre indiqué \n \n -`de plus certaines commandes sont en cours de dev...`")
+
+client.once("ready", () => {
+  console.log("Linkbot est en ligne, tout roule");
+});
+
+client.on("message", async message => {
+  if (message.author.bot) return;
+  //initialisation des args
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  //Commandes
+
+  //test
+  if (command === "test") {
+    message.channel.send(embedt);
+    return;
+  }
+
+  //help
+  else if (command === "help") {
+    message.channel.send(embedh);
+    return;
+  }
+
+  //commande en dev
+  else if (command === "jojo") {
+    message.channel.send("commande en cours de dev, cheh \nD'ailleurs un peu gay en vrai le manga");
+    return;
+
+  }
+
+  //commande troll
+  else if (command === "punchline") {
+    message.channel.send("Tg salarabe la commande est en cours de dev retourne jouer à ta nintendaucshwitz");
+    return;
+  }
+  //commande musique
+  else if (command === "play") {
+    if (message.member.voice.channel) {
+      message.member.voice.channel.join().then(connection => {
+
+        let dispatcher = connection.play(ytdl(args[0], { quality: "highestaudio" }));
+
+        dispatcher.on("finish", () => {
+          dispatcher.destroy();
+        });
+
+        dispatcher.on("error", err => {
+          console.log("erreur dans le disparch-truc :" + err);
+        });
+      }).catch(err => {
+        message.reply("meh j'arrive pas à join le voc heuu :" + err);
+        console.log("j'arrive pas à me co mon fuhrer");
+      })
+      message.reply("jsuis en voc c'est bon");
+    }
+    else {
+      message.reply("Va en voc débile");
+    }
+    return;
+  };
+});
+
+client.login(process.env.token)
