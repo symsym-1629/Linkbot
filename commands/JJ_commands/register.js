e851bcc94ebf6fe859d51a6871d058fadb964717
symsym <sym.playing@gmail.com>
2023-08-04 23:48:34 +0200

    valid fiches channel

--- commands/JJ_commands/register.js
+++ commands/JJ_commands/register.js
@@ -103,7 +103,7 @@ module.exports = {
 
         if (!interaction.member.roles.cache.has(process.env.validatorId)) return interaction.editReply({content:`Vous n'avez pas la permission d'utiliser cette commande !`});
         try {
-            const perso = await Perso.create({
+            const element = await Perso.create({
                 name: name,
                 race: race,
                 affiliation: affiliation,
@@ -120,7 +120,30 @@ module.exports = {
                 userid: user.id,
                 dead: false
             });
-        await interaction.editReply({content:`Le personnage ${perso.name} a bien été enregistré !`});
+            await interaction.editReply({content:`Le personnage ${element.name} a bien été enregistré !`});
+
+            let embed = new Discord.EmbedBuilder()
+                .setColor('Random')
+                .setTitle(element.name)
+                element.imagelink ? embed.setThumbnail(element.imagelink) : console.log("no tmb")
+                embed.setAuthor({ name: `appartient à ${user.username}`, iconURL: user.displayAvatarURL()})
+                .setDescription(`race : ${element.race} \n affiliation : ${element.affiliation}`)
+                .addFields({ name: 'Capacités', value: `${element.cplevel ? `- Chaos / Paix : ${element.cplevel} \n` : "\n"}- Hamon : ${element.hamonlevel ? element.hamonlevel : "Non maitrisé"} \n- Rotation : ${element.rotationlevel ? element.rotationlevel : "Non maitrisé"} \n- Vampirisme : ${element.vampirismelevel ? element.vampirismelevel : "Non maitrisé"}` })
+                if (element.standname) {
+                    let args = element.standstats.split('-');
+                    embed.addFields(
+                        { name: `Stand : ${element.standname}`, value: '\u200B' },
+                        { name: 'Stats', value: `- Force : ${args[0]} \n- Vitesse : ${args[1]} \n- Portée : ${args[2]} \n- Durabilité : ${args[3]} \n- Précision : ${args[4]} \n- Potentiel : ${args[5]}`, inline: true },
+                        { name: 'Requiem', value: element.hasrequiem ? 'Oui' : 'Non', inline: true },
+                        { name: 'Over Heaven', value: element.hasoverheaven ? 'Oui' : 'Non', inline: true },
+                    )
+                };
+                embed.addFields({ name: 'Lien vers la fiche', value: element.ficheurl })
+                .setFooter({ text: `ID : ${element.id}` });
+
+            let guild = interaction.guild;
+            let validChannel = await guild.channels.fetch("1008659272676163658");
+            await validChannel.send({embeds: [embed]});
         } catch (error) {
             console.error(error);
             if (error.name === 'SequelizeUniqueConstraintError') {
